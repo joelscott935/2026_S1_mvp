0c170b7c8a41c6d89f299bea421fc799391ccf0e
joel <js08082002@gmail.com>
2026-02-17 15:32:05 +0800

    floor and hazard

---
+++ 2026_S1_mvp/Assets/scripts/FirstPersonController(3).cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float sprintSpeed = 5.0f;
+    public float gravity = 3.5f;
+    public float jumpForce = 0.1f;
+
+    private float currentSpeed = 0;
+    private float velocity = 0;
+    private CharacterController controller;
+    private Vector3 motion;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        motion = Vector3.zero;
+
+        if (controller.isGrounded == true)
+        {
+            velocity = -gravity * Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.Space) == true)
+            {
+                velocity = jumpForce;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftShift) == true)
+            {
+                //check to see speed is not equal to sprint speed
+                if (currentSpeed != sprintSpeed)
+                {
+                    currentSpeed = sprintSpeed;
+                }
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftShift) == true)
+            {
+                //change to walk speed
+                if (currentSpeed != speed)
+                {
+                    currentSpeed = speed;
+                }
+            }
+        }
+        else
+        {
+            velocity -= gravity * Time.deltaTime;
+        }
+        ApplyMovement();
+    }
+
+    void ApplyMovement()
+    {
+        motion += transform.forward * Input.GetAxisRaw("Vertical") * currentSpeed * Time.deltaTime;
+        motion += transform.right * Input.GetAxisRaw("Horizontal") * currentSpeed * Time.deltaTime;
+        motion.y += velocity;
+        controller.Move(motion);
+    }
+}
+
---
+++ 2026_S1_mvp/Assets/scripts/MouseLook(3).cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float sensitivity = 2.5f;
+    public float drag = 1.5f;
+
+    private Transform character;
+    private Vector2 mouseDir;
+    private Vector2 smoothing;
+    private Vector2 result;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        character = transform.root;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseDir = new Vector2(Input.GetAxisRaw("Mouse X") * sensitivity, Input.GetAxisRaw("Mouse Y") * sensitivity);
+        smoothing = Vector2.Lerp(smoothing, mouseDir, 1 / drag);
+        result += smoothing;
+        result.y = Mathf.Clamp(result.y, -70, 70);
+
+        transform.localRotation = Quaternion.AngleAxis(-result.y, Vector3.right);
+        character.rotation = Quaternion.AngleAxis(result.x, character.up);
+
+    }
+}
